08c41a48d7c99ceeac8ba5dfb639272e267af83b
kortschak <dan.kortschak@adelaide.edu.au>
2014-06-11 15:18:16 +0930

    Add meaning to bin calculation source

--- bam.go
+++ bam.go
@@ -58,46 +58,66 @@ const (
 	maxUint32 = int64(^uint32(0))
 )
 
+const wordBits = 29
+
 func validInt16(i int) bool    { return minInt16 <= i && i <= maxInt16 }
 func validInt32(i int) bool    { return minInt32 <= i && i <= maxInt32 }
 func validUint16(i int) bool   { return 0 <= i && i <= maxUint16 }
 func validUint32(i int) bool   { return 0 <= i && int64(i) <= maxUint32 }
-func validPos(i int) bool      { return 0 <= i && i <= 1<<29-1 }
-func validTmpltLen(i int) bool { return -(1<<29) <= i && i <= 1<<29-1 }
-func validLen(i int) bool      { return 1 <= i && i <= 1<<29-1 }
+func validPos(i int) bool      { return 0 <= i && i <= 1<<wordBits-1 }
+func validTmpltLen(i int) bool { return -(1<<wordBits) <= i && i <= 1<<wordBits-1 }
+func validLen(i int) bool      { return 1 <= i && i <= 1<<wordBits-1 }
+
+const nextBinShift = 3
+
+const (
+	level0 = uint16(((1 << (iota * nextBinShift)) - 1) / 7)
+	level1
+	level2
+	level3
+	level4
+	level5
+)
+
+const (
+	level0Shift = wordBits - (iota * nextBinShift)
+	level1Shift
+	level2Shift
+	level3Shift
+	level4Shift
+	level5Shift
+)
 
 // calculate bin given an alignment covering [beg,end) (zero-based, half-close-half-open)
 func reg2bin(beg, end int) uint16 {
 	end--
 	switch {
-	case beg>>14 == end>>14:
-		return uint16(((1<<15)-1)/7 + (beg >> 14))
-	case beg>>17 == end>>17:
-		return uint16(((1<<12)-1)/7 + (beg >> 17))
-	case beg>>20 == end>>20:
-		return uint16(((1<<9)-1)/7 + (beg >> 20))
-	case beg>>23 == end>>23:
-		return uint16(((1<<6)-1)/7 + (beg >> 23))
-	case beg>>26 == end>>26:
-		return uint16(((1<<3)-1)/7 + (beg >> 26))
+	case beg>>level5Shift == end>>level5Shift:
+		return level5 + uint16(beg>>level5Shift)
+	case beg>>level4Shift == end>>level4Shift:
+		return level4 + uint16(beg>>level4Shift)
+	case beg>>level3Shift == end>>level3Shift:
+		return level3 + uint16(beg>>level3Shift)
+	case beg>>level2Shift == end>>level2Shift:
+		return level2 + uint16(beg>>level2Shift)
+	case beg>>level1Shift == end>>level1Shift:
+		return level1 + uint16(beg>>level1Shift)
 	}
-	return 0
+	return level0
 }
-
-const maxBin = (((1 << 18) - 1) / 7)
 
 // calculate the list of bins that may overlap with region [beg,end) (zero-based)
 func reg2bins(beg, end int) []uint16 {
 	end--
-	list := []uint16{0}
+	list := []uint16{level0}
 	for _, r := range []struct {
 		offset, shift uint16
 	}{
-		{1, 26},
-		{9, 23},
-		{73, 20},
-		{585, 17},
-		{4681, 14},
+		{level1, level1Shift},
+		{level2, level2Shift},
+		{level3, level3Shift},
+		{level4, level4Shift},
+		{level5, level5Shift},
 	} {
 		for k := r.offset + uint16(beg>>r.shift); k <= r.offset+uint16(end>>r.shift); k++ {
 			list = append(list, k)
