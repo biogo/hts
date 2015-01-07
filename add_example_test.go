323ee60c0c5bd08a1b3e0ebbea115faac424577f
kortschak <dan.kortschak@adelaide.edu.au>
2015-01-07 13:08:00 +1030

    Add example for Index.Add
    This is essentially the vanilla BAM indexing code.

---
+++ add_example_test.go
@@ -0,0 +1,41 @@
+// Copyright ©2015 The bíogo.bam Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package bam_test
+
+import (
+	"io"
+	"log"
+	"os"
+
+	"code.google.com/p/biogo.bam"
+)
+
+func ExampleIndex_Add() {
+	// Create a BAI for the BAM read from standard in and write it to standard out.
+	br, err := bam.NewReader(os.Stdin)
+	if err != nil {
+		log.Fatalf("failed to open BAM: %v", err)
+	}
+
+	var bai bam.Index
+	for {
+		r, err := br.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("failed to read BAM record: %v", err)
+		}
+		err = bai.Add(r, br.LastChunk())
+		if err != nil {
+			log.Fatalf("failed to add record to BAM index: %v", err)
+		}
+	}
+
+	err = bam.WriteIndex(os.Stdout, &bai)
+	if err != nil {
+		log.Fatalf("failed to write BAM index: %v", err)
+	}
+}
