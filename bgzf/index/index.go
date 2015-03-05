39bea1c3249b19f1043af288cbd6a2bdd094f568
kortschak <dan.kortschak@adelaide.edu.au>
2015-03-06 10:28:43 +1030

    Add package doc-ish

---
+++ bgzf/index/index.go
@@ -0,0 +1,6 @@
+// Copyright ©2015 The bíogo.bam Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package index implement CSI and Tabix BGZF indexing.
+package index
