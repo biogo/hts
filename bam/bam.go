b9e779600485a5774272f58f91301674f5619e27
kortschak <dan.kortschak@adelaide.edu.au>
2015-03-06 10:37:59 +1030

    Fix bam package doc

--- bam/bam.go
+++ bam/bam.go
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package bam implements BAM file format reading and writing. The SAM format
-// is described in the SAM specification.
+// Package bam implements BAM file format reading, writing and indexing.
+// The BAM format is described in the SAM specification.
 //
 // http://samtools.github.io/hts-specs/SAMv1.pdf
 package bam
